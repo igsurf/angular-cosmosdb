27b8764d4f72036265cf8110f18f2e5f716f26c3
Igor Abreu Araujo <igor.abreu.araujo@avanade.com>
2017-07-24 17:33:07 -0300

    Adicionando body-parse e express, configurando launch para configuração de debug.

---
+++ src/server/routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('heroes', (req, res) =>{
+    res.send(200, [
+        { "id": 10, "name": "StarLord", "saying": "oh yeah"}
+    ])
+});
+
+module.exports = router;
