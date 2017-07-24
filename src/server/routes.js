2b3ec10c6d7c4c20dbf893e54b37cca1e387bc79
Igor Abreu Araujo <igor.abreu.araujo@avanade.com>
2017-07-24 20:03:07 -0300

    Correção da rota Heroes

--- src/server/routes.js
+++ src/server/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('heroes', (req, res) =>{
+router.get('/heroes', (req, res) =>{
     res.send(200, [
         { "id": 10, "name": "StarLord", "saying": "oh yeah"}
     ])
